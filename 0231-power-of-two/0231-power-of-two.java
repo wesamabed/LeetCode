42deccf83c3f6a161e6b2f3a78551abc7845a681
wesamabed <68119943+wesamabed@users.noreply.github.com>
2025-04-22 18:06:55 +0300

    Time: 0 ms (100%), Space: 40.4 MB (98.43%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.java
+++ 0231-power-of-two/0231-power-of-two.java
@@ -1,11 +1,5 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
-        if (n <= 0) return false;
-        if (n == 1) return true ;
-        while (n != 2 ) {
-            if ( n % 2 != 0) return false;
-            n = n / 2;
-        }
-        return true;
+        return (n > 0) && (n & (n - 1)) == 0;
     }
 }
