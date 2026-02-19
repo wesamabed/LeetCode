335be5a924817597b7cf0981112929ba8c66eb71
wesam abed <68119943+wesamabed@users.noreply.github.com>
2026-02-19 23:04:20 +0200

    Time: 15 ms (30.51%), Space: 49.9 MB (49.79%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,17 +1,24 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        Map<List<Integer>, List<String>> map = new HashMap<>();
+        Map<String, List<String>> map = new HashMap<>();
         for (String s : strs){
-            char [] chars = s.toCharArray();
-            List<Integer> count = new ArrayList<>(Collections.nCopies(26, 0));
-            for (char c : chars){
-                int index = c - (int) 'a';
-                count.set(index, count.get(index) + 1);
-
+            int [] count = new int[26];
+            for (int i = 0 ; i < s.length(); i++){
+                int index = s.charAt(i) - (int) 'a';
+                count[index] += 1;
             }
-            map.computeIfAbsent(count, c -> new ArrayList<>()).add(s);
+            String key = claculateKey(count);
+            map.computeIfAbsent(key, k -> new ArrayList<>()).add(s);
         }
         return new ArrayList<>(map.values());
+    }
+
+    public String claculateKey(int[] count){
+        StringBuilder st = new StringBuilder();
+        for (int i = 0 ; i < count.length ; i++){
+            st.append(count[i]).append('#');
+        }
+        return st.toString();
 
     }
 }
