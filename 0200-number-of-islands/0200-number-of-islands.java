4e4fec67ab2a2a9bce2dcb5d19c2dfd2b7b0d917
wesam abed <68119943+wesamabed@users.noreply.github.com>
2026-02-23 22:17:34 +0200

    Time: 8 ms (11.28%), Space: 51.5 MB (84.82%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,46 +1,31 @@
 class Solution {
     public int numIslands(char[][] grid) {
+        if(grid == null || grid.length == 0) return 0;
+        int[][] directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
         int row = grid.length;
         int column = grid[0].length;
-        int max = 0;
+        int islands = 0;
         for (int i = 0 ; i < row ; i++){
-            for (int j = 0 ; j < column ; j++){
+            for (int j = 0; j < column ; j++){
                 if(grid[i][j] != '1') continue;
                 Queue<int[]> queue = new ArrayDeque<>();
                 queue.offer(new int[]{i, j});
-                max += 1;
                 grid[i][j] = '0';
+                islands ++;
                 while(!queue.isEmpty()){
-                    int levelSize = queue.size();
-                    for (int k = 0 ; k < levelSize; k++){
-                        int [] current = queue.poll();
-                        int curRow = current[0];
-                        int curCol = current[1];
-                        boolean left = curCol - 1 >= 0;
-                        boolean right = curCol + 1 < column;
-                        boolean up = curRow - 1 >= 0;
-                        boolean down = curRow + 1 < row;
-                        if (right && grid[curRow][curCol + 1] == '1'){
-                            queue.offer(new int[]{curRow, curCol + 1});
-                            grid[curRow][curCol + 1] = '0';
-                        }
-                        if (left && grid[curRow][curCol - 1] == '1'){
-                            queue.offer(new int[]{curRow, curCol - 1});
-                            grid[curRow][curCol - 1] = '0';
-                        }
-                        
-                        if (down && grid[curRow + 1][curCol] == '1'){
-                            queue.offer(new int[]{curRow + 1,curCol});
-                            grid[curRow + 1][curCol] = '0';
-                        }
-                        if (up && grid[curRow - 1][curCol] == '1'){
-                            queue.offer(new int[]{curRow - 1,curCol});
-                            grid[curRow - 1][curCol] = '0';
+                    int [] curr = queue.poll();
+                    for (int[] dir : directions){
+                        int newRow = curr[0] + dir[0];
+                        int newCol = curr[1] + dir[1];
+                        if(newRow >= 0 && newRow < row && newCol >= 0 && newCol < column && grid[newRow][newCol] == '1'){
+                            queue.offer(new int[]{newRow, newCol});
+                            grid[newRow][newCol] = '0';
                         }
                     }
+
                 }
             }
         }
-        return max;
+        return islands;
     }
 }
