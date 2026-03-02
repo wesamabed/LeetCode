27453661466300d491e6fc990f71b4cb515a1e5b
wesam abed <68119943+wesamabed@users.noreply.github.com>
2026-03-02 22:33:55 +0200

    Time: 5 ms (82.02%), Space: 46.5 MB (97.47%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.java
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -1,30 +1,31 @@
 class Solution {
     public int[] findOrder(int numCourses, int[][] prerequisites) {
         List<List<Integer>> adj = new ArrayList<>(numCourses);
-        int[] visited = new int[numCourses];
+        int[] inDegree = new int[numCourses];
         int[] order = new int[numCourses];
-        int[] count = {0};
+        int[] count = {numCourses - 1};
         for(int i = 0; i < numCourses; i++) adj.add(i, new ArrayList<>());
         for(int[] prerequisite: prerequisites){
-            adj.get(prerequisite[0]).add(prerequisite[1]);
+            adj.get(prerequisite[1]).add(prerequisite[0]);
+            inDegree[prerequisite[0]]++;
         } 
-        for (int i = 0; i < numCourses; i++){
-            if(!canFinish(i, adj, visited, order, count)) return new int[0];
+        int counter = 0;
+        Queue<Integer> queue = new ArrayDeque<>(numCourses);
+        for(int i = 0; i < numCourses; i++){
+            if(inDegree[i] == 0) queue.offer(i);
         }
-        return order;
+        while(!queue.isEmpty()){
+            int current = queue.poll();
+            order[counter] = current;
+            counter++;
+            List<Integer> neighbors = adj.get(current);
+            for(int neighbor: neighbors){
+                inDegree[neighbor]--;
+                if(inDegree[neighbor] == 0) queue.offer(neighbor);
+            }
+        }
+
+        return counter == numCourses ? order : new int[0];
     }
 
-    private boolean canFinish(int course, List<List<Integer>> adj, int[] visited, int[] order, int[] count){
-        if(visited[course] == 2) return true;
-        if(visited[course] == 1) return false;
-        visited[course] = 1;
-        List<Integer> neighbors = adj.get(course);
-        for(int neighbor: neighbors){
-            if(!canFinish(neighbor, adj, visited, order, count)) return false;
-        }
-        visited[course] = 2;
-        order[count[0]] = course;
-        count[0] ++;
-        return true;
-    }
 }
