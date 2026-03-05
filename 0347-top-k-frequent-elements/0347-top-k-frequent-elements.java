e566467ba787bebb2a5a6a2cb0ade48a5f497dba
wesam abed <68119943+wesamabed@users.noreply.github.com>
2026-03-06 00:02:10 +0200

    Time: 12 ms (83.72%), Space: 50.5 MB (9.79%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -4,15 +4,24 @@ class Solution {
         for(int num: nums){
             map.put(num, map.getOrDefault(num, 0) + 1);
         }
-        PriorityQueue<Integer> minHeap = new PriorityQueue<>(k, (a, b) -> map.get(a) - map.get(b));
-        for(int key: map.keySet()){
-            minHeap.offer(key);
-            if(minHeap.size() > k) minHeap.poll();
+
+        List<List<Integer>> bucket = new ArrayList<>(nums.length + 1);
+        for(int i = 0; i < nums.length + 1; i++) bucket.add(i, new ArrayList<>());
+        for(Map.Entry<Integer, Integer> entry: map.entrySet()){
+            int key = entry.getKey();
+            int value = entry.getValue();
+            bucket.get(value).add(key);
         }
-        int[] result = new int[k];
-        for(int i = k - 1; i >= 0 ; i--){
-            result[i] = minHeap.poll();
+        List<Integer> result = new ArrayList<>();
+        for(int i = nums.length; i >= 0; i--){
+            if(result.size() >= k) break;
+            if(bucket.get(i).isEmpty()) continue;
+            result.addAll(bucket.get(i));
         }
-        return result;
+        int[] topK = new int[k];
+        for(int i = 0; i < k; i++){
+            topK[i] = result.get(i);
+        }
+        return topK;
     }
 }
