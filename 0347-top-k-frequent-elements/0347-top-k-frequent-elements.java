b6091fc9f2b9bfe633473a953785e1f42be43e16
wesam abed <68119943+wesamabed@users.noreply.github.com>
2026-03-05 23:36:41 +0200

    Time: 16 ms (39.19%), Space: 47.5 MB (91.01%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -4,7 +4,7 @@ class Solution {
         for(int num: nums){
             map.put(num, map.getOrDefault(num, 0) + 1);
         }
-        PriorityQueue<Integer> minHeap = new PriorityQueue<>((a, b) -> map.get(a) - map.get(b));
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>(k, (a, b) -> map.get(a) - map.get(b));
         for(int key: map.keySet()){
             minHeap.offer(key);
             if(minHeap.size() > k) minHeap.poll();
