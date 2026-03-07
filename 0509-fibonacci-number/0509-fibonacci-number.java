6ecf9680985bda2abdd4689290ae628e841b7eca
wesam abed <68119943+wesamabed@users.noreply.github.com>
2026-03-07 19:51:55 +0200

    Time: 0 ms (100%), Space: 42.3 MB (30.28%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,15 +1,12 @@
 class Solution {
     public int fib(int n) {
-        int[] memo = new int [n + 1];
         if(n <= 1) return n;
+        int[] memo = new int [n + 1];
         memo[1] = 1;
-        return calc(n, memo);
-    }
-    private int calc(int n, int[] memo){
-        if(n == 0) return 0;
-        if(n == 1) return 1;
-        if(memo[n] != 0) return memo[n];
-        memo[n] = calc(n - 1, memo) + calc(n - 2, memo);
+        for(int i = 2; i <= n; i++){
+            memo[i] = memo[i-1] + memo[i-2];
+        }
         return memo[n];
     }
+
 }
