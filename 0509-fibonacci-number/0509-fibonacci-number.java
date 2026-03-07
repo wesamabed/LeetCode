13d59451ba01bc619b2e12ba331ed7299a0e74dc
wesam abed <68119943+wesamabed@users.noreply.github.com>
2026-03-07 19:49:36 +0200

    Time: 0 ms (100%), Space: 42.4 MB (13.57%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -8,6 +8,7 @@ class Solution {
     private int calc(int n, int[] memo){
         if(n == 0) return 0;
         if(n == 1) return 1;
+        if(memo[n] != 0) return memo[n];
         int fib = calc(n - 1, memo) + calc(n - 2, memo);
         memo[n] = fib;
         return fib;
