8e9f0ded6ba4c3d3acbb935190015ea9c5731229
wesam abed <68119943+wesamabed@users.noreply.github.com>
2026-03-07 20:13:59 +0200

    Time: 0 ms (100%), Space: 42.1 MB (72.37%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,14 +1,12 @@
 class Solution {
     public int climbStairs(int n) {
         int[] steps = new int[n + 1];
-        if(n <= 1) return n;
-        return calc(n, steps);
-    }
-    private int calc(int n, int[] steps){
-        if(n == 1) return 1;
-        if(n == 2) return 2;
-        if(steps[n] != 0) return steps[n];
-        steps[n] = calc(n - 1, steps) + calc(n - 2, steps);
+        if(n <= 2) return n;
+        steps[1] = 1;
+        steps[2] = 2;
+        for(int i = 3; i <= n; i++){
+            steps[i] = steps[i - 1] + steps[i - 2];
+        } 
         return steps[n];
     }
 }
