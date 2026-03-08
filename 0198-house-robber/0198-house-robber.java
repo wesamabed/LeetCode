bff51ccf97d976a2d29307b9245978464116c1eb
wesam abed <68119943+wesamabed@users.noreply.github.com>
2026-03-09 01:34:54 +0200

    Time: 0 ms (100%), Space: 42.7 MB (73.04%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,17 +1,18 @@
 class Solution {
     public int rob(int[] nums) {
-        if(nums.length == 0) return 0;
-        int[] memo = new int[nums.length];
-        Arrays.fill(memo, -1);
-        return computeMaxProfit(nums, memo, nums.length - 1);
+        if(nums == null || nums.length == 0) return 0;
+        if(nums.length == 1) return nums[0];
+        int prev2 = nums[0];
+        int prev1 = Math.max(nums[0], nums[1]);
+        int current = prev1;
+        for(int i = 2; i < nums.length; i++){
+            int rob = nums[i] + prev2;
+            int skip = prev1;
+            current = Math.max(rob, skip);
+            prev2 = prev1;
+            prev1 = current;
+        }
+        return current;
     }
-    private int computeMaxProfit(int[] nums, int[] memo, int n){
-        if(n < 0) return 0;
-        if(n == 0) return nums[0];
-        if(memo[n] != -1) return memo[n];
-        int rop = nums[n] + computeMaxProfit(nums, memo, n - 2);
-        int skip = computeMaxProfit(nums, memo, n - 1);
-        memo[n] = Math.max(rop, skip);
-        return memo[n];
-    }
+
 }
