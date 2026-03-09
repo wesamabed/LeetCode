efe89f84a3babda892cfdc2c56f18ef5ed8e86d6
wesam abed <68119943+wesamabed@users.noreply.github.com>
2026-03-09 17:14:46 +0200

    Time: 5 ms (86.53%), Space: 45.5 MB (80.62%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,28 +1,15 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        if (s == null || s.length() == 0) return 0;
-        
-        int left = 0; 
-        int right = 0; 
-        int max = 0;
+        if(s == null || s.length() == 0) return 0;
+        int left = 0; int max = 0;
         Map<Character, Integer> map = new HashMap<>();
-        
-        while (right < s.length()) {
+        for(int right = 0; right < s.length(); right++){
             char c = s.charAt(right);
-            
-            if (map.containsKey(c)) {
-                // If we've seen it, jump 'left' to the right of the old position
+            if(map.containsKey(c)){
                 left = Math.max(left, map.get(c) + 1);
             }
-            
-            // ALWAYS update the map with the newest position
             map.put(c, right);
-            
-            // ALWAYS move right forward
-            right++;
-            
-            // Calculate max (using your 'right' logic which is already 1-step ahead)
-            max = Math.max(max, right - left);
+            max = Math.max(max, right - left + 1);
         }
         return max;
     }
