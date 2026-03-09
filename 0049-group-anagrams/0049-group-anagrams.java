2d882937a5bb5f5ac83b955dc5e5eaafec39b7a0
wesam abed <68119943+wesamabed@users.noreply.github.com>
2026-03-09 19:31:00 +0200

    Time: 18 ms (25.69%), Space: 50.1 MB (25%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,24 +1,24 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
+        List<List<String>> list = new ArrayList<>();
+        if(strs == null || strs.length == 0) return list;
         Map<String, List<String>> map = new HashMap<>();
-        for (String s : strs){
-            int [] count = new int[26];
-            for (int i = 0 ; i < s.length(); i++){
-                int index = s.charAt(i) - (int) 'a';
-                count[index] += 1;
+        for(String s: strs){
+            int[] chars = new int[26];
+            for(int i = 0; i < s.length(); i++){
+                chars[s.charAt(i) - 'a'] += 1;
             }
-            String key = claculateKey(count);
+            StringBuilder sb = new StringBuilder();
+            for(int num: chars){
+                sb.append(num);
+                sb.append('#');
+            }
+            String key = sb.toString();
             map.computeIfAbsent(key, k -> new ArrayList<>()).add(s);
         }
-        return new ArrayList<>(map.values());
-    }
-
-    public String claculateKey(int[] count){
-        StringBuilder st = new StringBuilder();
-        for (int i = 0 ; i < count.length ; i++){
-            st.append(count[i]).append('#');
+        for(List<String> anag: map.values()){
+            list.add(anag);
         }
-        return st.toString();
-
+        return list;
     }
 }
