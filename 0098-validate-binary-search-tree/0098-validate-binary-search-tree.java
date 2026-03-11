9846a1e31904ba67901c38893857e27dc6d2fb70
wesam abed <68119943+wesamabed@users.noreply.github.com>
2026-03-11 22:41:40 +0200

    Time: 0 ms (100%), Space: 45.1 MB (77.36%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -15,14 +15,14 @@
  */
 class Solution {
     public boolean isValidBST(TreeNode root) {
-        return bst(root, Long.MIN_VALUE, Long.MAX_VALUE);
+        if(root == null) return false;
+        long max = Long.MAX_VALUE;
+        long min = Long.MIN_VALUE;
+        return bst(root, min, max);
     }
     private boolean bst(TreeNode root, long min, long max){
         if(root == null) return true;
         if(root.val <= min || root.val >= max) return false;
         return bst(root.left, min, root.val) && bst(root.right, root.val, max);
-
     }
-
-
 }
