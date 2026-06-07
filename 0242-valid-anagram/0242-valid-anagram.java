ee2ab6595878ee0c356b5c90d7fa13375ac6675c
wesam abed <68119943+wesamabed@users.noreply.github.com>
2026-06-07 21:12:23 +0300

    Time: 6 ms (44.92%), Space: 44.3 MB (81.87%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,13 +1,13 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if(s == null || t == null || s.length() != t.length()) return false;
-        int[] anagram = new int[26];
+        if(s.length() != t.length()) return false;
+        int[] charCounts = new int[26];
         for(int i = 0; i < s.length(); i++){
-            anagram[s.charAt(i) - 'a'] += 1;
+            charCounts[s.charAt(i) - 'a']++;
+            charCounts[t.charAt(i) - 'a']--;
         }
-        for(char c : t.toCharArray()){
-            if(anagram[c - 'a'] == 0) return false;
-            anagram[c - 'a']--;
+        for(int num: charCounts){
+            if(num != 0) return false;
         }
         return true;
     }
