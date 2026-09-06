d05c4a790f614302fbf74b8a411586c69dd1fc01
wesam abed <68119943+wesamabed@users.noreply.github.com>
2026-09-06 20:26:54 +0300

    Time: 2 ms (44.57%), Space: 94.3 MB (70.85%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,5 +1,6 @@
 class Solution {
     public int maxProfit(int[] prices) {
+        if (prices == null || prices.length == 0) return 0;
         int max = 0;
         int currPrice = prices[0];
         for(int i = 1; i < prices.length; i++){
