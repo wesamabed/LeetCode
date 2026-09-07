03aa78fd5536b4a9a58a3a5dc2a966c5e9998d97
wesam abed <68119943+wesamabed@users.noreply.github.com>
2026-09-07 20:06:19 +0300

    Time: 2 ms (91.8%), Space: 47.7 MB (76.03%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,16 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int left = 0;
+        int right = 0;
+        while(right < nums.length){
+            if(nums[right] == 0) right++;
+            else{
+                int temp = nums[right];
+                nums[right] = nums[left];
+                nums[left] = temp;
+                left++;
+                right++;
+            }
+        }
+    }
+}
