bcbc116da9aa241c629292dd448577101cb221a1
wesam abed <68119943+wesamabed@users.noreply.github.com>
2026-09-09 00:26:13 +0300

    Time: 0 ms (100%), Space: 47.2 MB (10.39%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
@@ -18,6 +18,6 @@ class Solution {
         if(root == null) return 0;
         int left = maxDepth(root.left);
         int right = maxDepth(root.right);
-        return Math.max(left, right) + 1;
+        return Math.max(left +1 , right+1);
     }
 }
